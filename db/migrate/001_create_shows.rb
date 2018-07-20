b7002a1d5aa2d72871e6abc3607a541d95822db7
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-20 11:37:57 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,9 @@
 class CreateShows < ActiveRecord::Migration
-
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating  
+  end 
 end 
-
-
-
-
-
-name, network, day, and rating columns. name, network, 
-and day have a datatype of string, and rating has a datatype of integer.
